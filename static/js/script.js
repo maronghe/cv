f245d182a1c6eb646d7060ee94ce505936c5ffa6
Rong He Ma RongHe <marhe@cn.ibm.com>
2018-06-20 21:49:52 +0800

    commit

--- static/js/script.js
+++ static/js/script.js
@@ -21,13 +21,13 @@ jQuery(document).ready(function($) {
     $(".right-paragraph p").attr('contenteditable', 'true');
     $("#username").attr('contenteditable', 'true');
     $("#persona-tag").attr('contenteditable', 'true');
-    $(".info-unit ul li").append('<span class="item-remove"><i class="iconfont icon-delete"></i></span>');
-    
+    //$(".info-unit ul li").append('<span class="item-remove"><i class="iconfont icon-delete"></i></span>');
+    /*
     $(".info-unit").filter(function(index) {
         return ($(this).children('ul').length);
     }).children("h2").append('<span class="item-add"><i class="iconfont icon-playlistadd"></i></span>');
-
-    $(".info-unit h2").append('<span class="unit-remove"><i class="iconfont icon-delete"></i></span>');
+*/
+    //$(".info-unit h2").append('<span class="unit-remove"><i class="iconfont icon-delete"></i></span>');
     
 
     var portrait_modal = $('[data-remodal-id=portrait-modal]').remodal();
@@ -58,11 +58,11 @@ jQuery(document).ready(function($) {
 
     $('.info-unit ul li').hover(function() {
         /* Stuff to do when the mouse enters the element */
-        $(this).children('.item-remove').css('visibility', 'visible');
+      //  $(this).children('.item-remove').css('visibility', 'visible');
 
     }, function() {
         /* Stuff to do when the mouse leaves the element */
-        $(this).children('.item-remove').css('visibility', 'hidden');
+        //$(this).children('.item-remove').css('visibility', 'hidden');
     });
 
     $('.unit-remove').click(function(event) {
@@ -70,7 +70,7 @@ jQuery(document).ready(function($) {
     });
 
     $('.item-remove').click(function(event) {
-        $(this).closest("li").remove();
+    //    $(this).closest("li").remove();
     });
 
     $('.item-add').click(function(event) {
@@ -83,8 +83,8 @@ jQuery(document).ready(function($) {
 
     });
     $("progress").click(function(event) {
-        $(this).attr("value", event.offsetX/$(this).width()/0.8 * $(this).attr("max"));
+        //$(this).attr("value", event.offsetX/$(this).width()/0.8 * $(this).attr("max"));
     });
-    // $("*").removeAttr('contenteditable');
+    $("*").removeAttr('contenteditable');
 
 });
